24fb8820c165c3601e80d7216e3c2549678c37da
Stefany Noguera <stefanynoguerar@gmail.com>
2024-11-12 18:08:34 +0100

    fixed issues for peloying

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -196,8 +196,8 @@ export default function Home() {
   return (
     <div className="snap-y snap-mandatory h-screen w-screen overflow-y-scroll">
       <div className="snap-start h-screen bg-gradient-to-b from-gray to-black">
-        <div className='flex justify-between pl-7'>
-          <div className='relative h-20 w-24 md:h-24 md:w-28 lg:w-36'>
+        <div className="flex justify-between pl-7">
+          <div className="relative h-20 w-24 md:h-24 md:w-28 lg:w-36">
             <Image
               src="/logo.png"
               layout="fill"
@@ -205,27 +205,27 @@ export default function Home() {
               alt="Logo"
             />
           </div>
-          <div className='flex items-top pt-4 md:pt-3 pr-5'>
-            <h3 className='md:p-4 p-2 text-xl md:text-2xl'>
+          <div className="flex items-top pt-4 md:pt-3 pr-5">
+            <h3 className="md:p-4 p-2 text-xl md:text-2xl">
               <button onClick={handleClickCatalog} className='text-beige'>Catalog</button>
             </h3>
-            <h3 className='md:p-4 p-2 text-xl md:text-2xl'>
-              <button onClick={handleClickCar} className='text-beige'>Find your Car</button>
+            <h3 className="md:p-4 p-2 text-xl md:text-2xl">
+              <button onClick={handleClickCar} className="text-beige">Find your Car</button>
             </h3>
-            <h3 className='md:p-4 p-2 text-xl md:text-2xl'>
+            <h3 className="md:p-4 p-2 text-xl md:text-2xl">
               <button onClick={handleClickContact} className='text-beige'>Contact</button>
             </h3>
           </div>
         </div>
 
-        <div className='flex flex-col items-center justify-center text-center pt-32 md:pt-20 lg:pt-1'>
+        <div className="flex flex-col items-center justify-center text-center pt-32 md:pt-20 lg:pt-1">
           <h1 className="text-beige break-sm break-md break-lg">
             The Parts You Need.
           </h1>
-          <h4 className='text-f-dark-gray break-title-sm break-title-md break-title-lg md:text-2xl lg:text-3xl pb-6 pt-3'>
+          <h4 className="text-f-dark-gray break-title-sm break-title-md break-title-lg md:text-2xl lg:text-3xl pb-6 pt-3">
             Discover premium parts for every make
           </h4>
-          <h4 className='text-beige bg-red p-3 px-4'>
+          <h4 className="text-beige bg-red p-3 px-4">
            <button onClick={handleClickCatalog}>Find Your Part</button>
           </h4>
         </div>
@@ -240,7 +240,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="snap-start h-screen bg-f-light-gray flex flex-col" id='catalog'>
+      <div className="snap-start h-screen bg-f-light-gray flex flex-col" id="catalog">
         <h1 className="text-center text-gray py-10 md:py-6 lg:py-4">Your Best Parts</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-10 px-10 lg:px-6 gap-4  flex-grow">
           {getVisibleProducts().map((product, index) => (
@@ -253,7 +253,7 @@ export default function Home() {
                   alt={product.name}
                 />
               </div>
-              <div className='flex justify-between'>
+              <div className="flex justify-between">
                 <h3 className="font-bold text-xl">{product.name}</h3>
                 <p className="text-gray">{product.price}</p>
               </div>
@@ -262,7 +262,7 @@ export default function Home() {
           ))}
         </div>
 
-        <div className='flex justify-center items-end md:pb-2 pb-5'>
+        <div className="flex justify-center items-end md:pb-2 pb-5">
           {currentPage > 1 && (
             <button
               onClick={goToPreviousPage}
@@ -283,7 +283,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="snap-start h-screen bg-gradient-to-b from-gray to-black flex items-center" id='find-car'>
+      <div className="snap-start h-screen bg-gradient-to-b from-gray to-black flex items-center" id="find-car">
         <div className="flex flex-col lg:flex-row-reverse lg:justify-between w-full items-center px-6 h-full">
           <div className="relative lg:w-1/2 w-[95%] lg:h-[90%] h-[57%] lg:ml-6 mt-6 lg:mt-0">
             <Image
@@ -306,37 +306,37 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="snap-start h-screen bg-f-light-gray flex flex-col" id='contact'>
+      <div className="snap-start h-screen bg-f-light-gray flex flex-col" id="contact">
 
-        <div className='flex items-center justify-center'>
-          <div className='text-gray lg:w-2/6 md:w-3/5 w-4/5'>
-            <h1 className='lg:pb-12 lg:pt-24 md:pt-28 md:pb-12 pt-32 pb-9'>Contact us</h1>
+        <div className="flex items-center justify-center">
+          <div className="text-gray lg:w-2/6 md:w-3/5 w-4/5">
+            <h1 className="lg:pb-12 lg:pt-24 md:pt-28 md:pb-12 pt-32 pb-9">Contact us</h1>
             <div>
               <h4>Your Name</h4>
               <input
                 type="text"
-                className='w-full text-gray border-b border-gray bg-transparent mb-4 p-2 focus:outline-none'
+                className="w-full text-gray border-b border-gray bg-transparent mb-4 p-2 focus:outline-none"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
               <h4>Email Address*</h4>
               <input
                 type="text"
-                className='w-full text-gray border-b border-gray bg-transparent mb-4 p-2 focus:outline-none'
+                className="w-full text-gray border-b border-gray bg-transparent mb-4 p-2 focus:outline-none"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
               <h4>Your Message</h4>
               <textarea
-                className='w-full text-gray border-b border-gray bg-transparent mb-4 p-2 focus:outline-none'
-                rows='4'
+                className="w-full text-gray border-b border-gray bg-transparent mb-4 p-2 focus:outline-none"
+                rows="4"
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
               ></textarea>
               {/* Show error message */}
               {error && <p className="text-red-500 text-sm">{error}</p>}
-              <div className='flex justify-end'>
-                <button className='text-beige bg-red p-3 px-4' onClick={handleSubmit}>
+              <div className="flex justify-end">
+                <button className="text-beige bg-red p-3 px-4" onClick={handleSubmit}>
                   <h4>Send Now</h4>
                 </button>
               </div>
@@ -344,8 +344,8 @@ export default function Home() {
           </div>
         </div>
 
-        <div className='flex justify-between flex-grow items-end px-5 lg:px-9 md:px-7'>
-          <div className='relative h-20 w-24  md:h-24 md:w-28 lg:w-36'>
+        <div className="flex justify-between flex-grow items-end px-5 lg:px-9 md:px-7">
+          <div className="relative h-20 w-24  md:h-24 md:w-28 lg:w-36">
             <Image
               src="/logo-white.png"
               layout="fill"
@@ -354,12 +354,12 @@ export default function Home() {
             />
           </div>
 
-          <div className='lg:flex lg:py-9 md:py-6 py-5'>
+          <div className="lg:flex lg:py-9 md:py-6 py-5">
             <Link href="/privacy-policy">
-              <h4 className='text-gray text-right lg:pr-6'>Privacy Policy</h4>
+              <h4 className="text-gray text-right lg:pr-6">Privacy Policy</h4>
             </Link>
             <Link href="/terms-and-conditions">
-              <h4 className='text-gray text-right'>Terms and Conditions</h4>
+              <h4 className="text-gray text-right">Terms and Conditions</h4>
             </Link>
           </div>
         </div>
@@ -369,7 +369,7 @@ export default function Home() {
           <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
             <div className="bg-white p-6 rounded-lg shadow-lg w-80 text-center">
               <h3 className="text-lg font-bold text-green-500 mb-4">Message Sent!</h3>
-              <p>Your message has been successfully sent. We'll get back to you shortly.</p>
+              <p>Your message has been successfully sent. We"ll get back to you shortly.</p>
               <div className="mt-8">
                 <button
                   className="text-beige bg-red p-2 px-6 rounded-md"
